f64999b7aa826fb7f86433c8f11493102eb1119d
j.fischer <j.fischer@huettinger.org>
2019-09-13 11:56:41 +0200

    Created 2d Array with numpy

--- matrixcode.py
+++ matrixcode.py
@@ -1,2 +1,10 @@
-msg = "Hello World"
-print(msg)
+import numpy as np
+
+mx_cols = 200
+mx_rows = 100
+
+# Create 2d Array filled with Int-Value 32 (32 = ASCII Space)
+matrix = np.full((mx_cols, mx_rows), 32, dtype=int)
+print(matrix)
+
+print(matrix[2,2])
